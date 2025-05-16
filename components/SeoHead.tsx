f2ff64ecca75fe8b899876ccb8e3d5839ffbcd9c
User <>
2025-05-16 10:17:30 +0800

    fix: 更新 Open Graph 標籤配置

--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -5,6 +5,7 @@ export default function SeoHead() {
   const siteDescription = '歡迎來到南科福氣教會，與我們一起經歷信仰、盼望與愛的同在';
   const siteUrl = 'https://nanke-blessing.vercel.app'; // 請替換為您的實際網址
   const siteImage = `${siteUrl}/images/logo-horizontal.png`; // 請替換為您的預覽圖片路徑
+  const siteImageAlt = '南科福氣教會標誌';
 
   return (
     <Head>
@@ -19,17 +20,22 @@ export default function SeoHead() {
       <meta property="og:url" content={siteUrl} />
       <meta property="og:title" content={siteTitle} />
       <meta property="og:description" content={siteDescription} />
-      <meta property="og:image" content={`${siteUrl}${siteImage}`} />
+      <meta property="og:image" content={siteImage} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:alt" content={siteImageAlt} />
       <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:locale" content="zh_TW" />
+      <meta property="og:locale:alternate" content="en_US" />
+      <meta property="fb:app_id" content="" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:url" content={siteUrl} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDescription} />
+      <meta name="twitter:image" content={siteImage} />
       <meta name="twitter:image" content={`${siteUrl}${siteImage}`} />
     </Head>
   );
