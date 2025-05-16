3fd4db353ae3cfeb74c134137323c9044ba37afa
User <>
2025-05-16 10:00:33 +0800

    fix: 恢復 vision-mission.tsx 文件內容並更新 SeoHead.tsx

--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -19,7 +19,6 @@ export default function SeoHead() {
       <meta property="og:url" content={siteUrl} />
       <meta property="og:title" content={siteTitle} />
       <meta property="og:description" content={siteDescription} />
-      <meta property="og:image" content={siteUrl + siteImage} />
       <meta property="og:image" content={`${siteUrl}${siteImage}`} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
